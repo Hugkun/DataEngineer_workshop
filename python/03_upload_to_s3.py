c55d380eea28123d2552ae290e0c252816eb4069
Hugkinkazu <k1.kinjo092@gmail.com>
2025-09-12 18:32:30 +0900

    パスの修正

--- python/03_upload_to_s3.py
+++ python/03_upload_to_s3.py
@@ -6,7 +6,7 @@ client = boto3.client('s3')
 
 print(client.list_buckets())
 
-Filename = '.csv/articles_data.csv'
-Bucket = 'templete-bucket' # 作成したbucke名に変更する
+Filename = './csv/articles_data.csv'
+Bucket = 'template-bucket' # 作成したbucket名に変更する
 Key = 'test/articles_data.csv'
 client.upload_file(Filename, Bucket, Key)
